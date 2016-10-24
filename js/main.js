72567e4016218379297cfeb5f430338c168bef36
Regina Galvez Chavez <reginagalvezchavez@MacBook-Pro-de-Regina.local>
2016-10-24 13:50:20 -0300

    cambios sin botón

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+//var porfa=document.getElementById("form-signin-heading");
+//porfa.placeholder= 
+
+document.getElementById("form-signin-heading").innerHTML="por favor inicia sesion";
+
+var input= document.getElementById("inputEmail");
+input.placeholder= "correo electronico" 
+
+var input=  document.getElementById("inputPassword");
+input.placeholder= "Contraseña"
+
+
+
+
